5aa5f9f190bf38e08a638ee40cd1ec57dd71db84
Qasim <69038179+Qasim345@users.noreply.github.com>
2021-12-22 13:05:51 +0430

    update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,3 @@
-window.addEventListener('beforeinstallprompt', e => {
-  console.log('beforeinstallprompt Event fired');
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  this.deferredPrompt = e;
-  return false;
-});
-// When you want to trigger prompt:
-this.deferredPrompt.prompt();
-this.deferredPrompt.userChoice.then(choice => {
-  console.log(choice);
-});
-this.deferredPrompt = null;
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
