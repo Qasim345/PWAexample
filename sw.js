e405b87719cea1e5b0a8a10b4672622c9d97fa6f
Qasim <69038179+Qasim345@users.noreply.github.com>
2021-12-22 15:13:54 +0430

    update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["/", "src/style.css", "/logo.png"])
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respndWith(
+    catches.match(e.request).then(respons => {
+      return respons || fetch(e.request)
+    })
+  )
+});
 let defferedPrompt;
 const addbtn = document.querySelector('.btn');
 
@@ -16,18 +31,4 @@ addbtn.addEventListener('click', event => {
         }
         defferedPrompt = null;
     })
-})
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("static").then(cache => {
-      return cache.addAll(["/", "src/style.css", "/logo.png"])
-    })
-  );
-});
-self.addEventListener("fetch", e => {
-  e.respndWith(
-    catches.match(e.request).then(respons => {
-      return respons || fetch(e.request)
-    })
-  )
 })
