c0b81c3c3ae4a2c2029e99f65c100c2746b23125
Qasim <69038179+Qasim345@users.noreply.github.com>
2021-12-22 15:16:15 +0430

    update sw.js

--- sw.js
+++ sw.js
@@ -13,22 +13,3 @@ self.addEventListener("fetch", e => {
     })
   )
 });
-let defferedPrompt;
-const addbtn = document.querySelector('.btn');
-
-window.addEventListener('beforeinstallprompt', event => {
-    event.preventDefault();
-    defferedPrompt = event
-    addbtn.style.display = 'block';
-});
-
-addbtn.addEventListener('click', event => {
-    defferedPrompt.prompt();
-
-    defferedPrompt.userChoice.then(choice => {
-        if(choice.outcome === 'accepted'){
-            console.log('user accepted the prompt')
-        }
-        defferedPrompt = null;
-    })
-})
